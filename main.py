b3e3216564e5205a6fe60298ea2972269d6f1340
Chevy8657 <160430687+Chevy8657@users.noreply.github.com>
2026-02-15 17:35:29 -0500

    Added greeting department

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ def home():
 @app.get("/test")
 def test():
     return {"message": "Local test successful"}
+@app.get("/greet/{name}")
+async def greet_user(name: str):
+    return {"message": f"Hello {name}, the Factory has processed your request.", "status": "Success"}
