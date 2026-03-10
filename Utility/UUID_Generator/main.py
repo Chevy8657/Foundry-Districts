fa18dd4fb42409b411462df3feeee3656a35c16f
Chevy <160430687+Chevy8657@users.noreply.github.com>
2026-03-10 16:40:49 -0400

    Update main.py

--- Utility/UUID_Generator/main.py
+++ Utility/UUID_Generator/main.py
@@ -1,28 +1,12 @@
-from fastapi import FastAPI
-import os
+from fastapi import APIRouter
+import uuid
 
-app = FastAPI()
-DB_FILE = "vault.txt"
+router = APIRouter()
 
-# Ensure the file exists so the machine doesn't crash
-if not os.path.exists(DB_FILE):
-    with open(DB_FILE, "w") as f:
-        f.write("")
+@router.get("/utility/uuid")
+async def generate_uuid():
+    new_uuid = str(uuid.uuid4())
 
-@app.get("/")
-def home():
-    return {"status": "The Factory is Online", "storage": "Permanent Ledger Active"}
-
-@app.post("/store-data")
-async def store(item: str):
-    with open(DB_FILE, "a") as f:
-        f.write(item + "\n")
-    return {"message": f"'{item}' permanently inked to ledger."}
-
-@app.get("/view-vault")
-async def view():
-    if os.path.exists(DB_FILE):
-        with open(DB_FILE, "r") as f:
-            items = f.read().splitlines()
-        return {"secured_items": items}
-    return {"secured_items": []}
+    return {
+        "generated_uuid": new_uuid
+    }
