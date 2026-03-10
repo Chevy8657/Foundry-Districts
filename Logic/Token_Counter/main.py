576fcceaca237dba7ee00c2feb8cfa9ce7e49b18
Chevy <160430687+Chevy8657@users.noreply.github.com>
2026-03-10 16:08:56 -0400

    Update main.py

--- Logic/Token_Counter/main.py
+++ Logic/Token_Counter/main.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/logic/word-count")
-async def word_count(text: str):
+@router.get("/logic/token-count")
+async def token_count(text: str):
     count = len(text.split())
     return {
         "input_text": text,
-        "word_count": count
+        "token_count": count
     }
