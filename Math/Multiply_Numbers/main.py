56cfac0efd00960a2f3773c75854c3875bf8725f
Chevy <160430687+Chevy8657@users.noreply.github.com>
2026-03-13 21:18:01 -0400

    Update main.py
    Fix Multiply Numbers capability

--- Math/Multiply_Numbers/main.py
+++ Math/Multiply_Numbers/main.py
@@ -4,8 +4,11 @@ router = APIRouter()
 
 @router.get("/math/multiply-numbers")
 def multiply_numbers(a: float, b: float):
+
+    result = a * b
+
     return {
         "a": a,
         "b": b,
-        "result": a * b
+        "result": result
     }
