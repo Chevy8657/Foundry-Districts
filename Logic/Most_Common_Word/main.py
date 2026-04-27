cd5b9d7b56a60fb223b11a8117834c05503f30e0
Chevy <160430687+Chevy8657@users.noreply.github.com>
2026-04-26 21:40:10 -0400

    Refactor most_common_word function to execute_tool

--- Logic/Most_Common_Word/main.py
+++ Logic/Most_Common_Word/main.py
@@ -3,23 +3,25 @@ from collections import Counter
 
 router = APIRouter()
 
-@router.get("/logic/most-common-word")
-def most_common_word(input_text: str):
-
+@router.get("/")
+def execute_tool(input_text: str):
     words = input_text.split()
 
     if not words:
-        return {
-            "input_text": input_text,
+        processed_data = {
             "most_common_word": None,
             "count": 0
         }
-
-    counter = Counter(words)
-    word, count = counter.most_common(1)[0]
+    else:
+        counter = Counter(words)
+        word, count = counter.most_common(1)[0]
+        processed_data = {
+            "most_common_word": word,
+            "count": count
+        }
 
     return {
+        "status": "SUCCESS",
         "input_text": input_text,
-        "most_common_word": word,
-        "count": count
+        "result": processed_data
     }
