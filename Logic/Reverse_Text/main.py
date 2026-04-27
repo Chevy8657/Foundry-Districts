392ffb3282f6791606bb03269ce829a51521f327
Chevy <160430687+Chevy8657@users.noreply.github.com>
2026-04-26 21:43:30 -0400

    Rename reverse_text to execute_tool and update route

--- Logic/Reverse_Text/main.py
+++ Logic/Reverse_Text/main.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/logic/reverse-text")
-def reverse_text(input_text: str):
-    reversed_text = input_text[::-1]
+@router.get("/")
+def execute_tool(input_text: str):
+    processed_data = input_text[::-1]
 
     return {
+        "status": "SUCCESS",
         "input_text": input_text,
-        "reversed_text": reversed_text
+        "result": processed_data
     }
