d123cc3be1a99a2a042057f9a693758a7765ed5e
Chevy <160430687+Chevy8657@users.noreply.github.com>
2026-04-26 21:57:58 -0400

    Refactor word count endpoint to new execute_tool

--- Logic/Word_Counter/main.py
+++ Logic/Word_Counter/main.py
@@ -1,21 +1,14 @@
 from fastapi import APIRouter
 
-# Create router object that the gateway imports
 router = APIRouter()
 
-
-@router.get("/logic/word-count")
-async def word_count(text: str):
-    """
-    Counts the number of words in a string.
-    Example:
-    /logic/word-count?text=The%20factory%20is%20online
-    """
-
+@router.get("/")
+async def execute_tool(text: str):
     words = text.split()
-    count = len(words)
+    processed_data = len(words)
 
     return {
+        "status": "SUCCESS",
         "input_text": text,
-        "word_count": count
+        "result": processed_data
     }
